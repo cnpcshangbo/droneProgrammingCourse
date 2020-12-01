1a4fe6ef529e8f9167269c3174f45707b9a3d75f
Bo Shang <cnpcshangbo@gmail.com>
2020-12-01 14:42:15 -0600

    added shell scripts

---
+++ scripts/launchDroneKitModule.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# declare STRING variable
+STRING="Launch DroneKit module."
+#print variable on a screen
+echo $STRING
+python ~/code/droneProgrammingCourse/dk/velocity_based_movement.py
---
+++ scripts/launchGirderDetectionModule.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# declare STRING variable
+STRING="Launch girder detection module."
+#print variable on a screen
+echo $STRING
+python ~/code/zed-opencv/python/zed-opencv.py
