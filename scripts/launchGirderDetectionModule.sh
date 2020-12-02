ba6d78e13758a1730654b0437fbd94ac297fc6a6
Bo Shang <cnpcshangbo@gmail.com>
2020-12-01 20:20:24 -0600

    added options of svo and cam

--- scripts/launchGirderDetectionModule.sh
+++ scripts/launchGirderDetectionModule.sh
@@ -3,4 +3,15 @@
 STRING="Launch girder detection module."
 #print variable on a screen
 echo $STRING
-python ~/code/zed-opencv/python/zed-opencv.py
+S1=$1
+echo S1= $S1
+S2="svo"
+echo S2= $S2
+if [[ "$S1" == "$S2" ]]; then
+	echo "working with a svo recording file."
+	sh ~/code/zed-opencv/test_light_from_ground.sh
+else 
+	echo "working with physical camera."
+	python ~/code/zed-opencv/python/zed-opencv.py
+fi 
+
