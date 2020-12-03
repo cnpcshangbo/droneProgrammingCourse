3b5869e6d71f7b4f9c5743f1638258b0d7df4dc6
Shang <UM-AD\bsr8w@r17gchen.managed.mst.edu>
2020-12-03 14:42:56 -0600

    added scd for bash.

--- scripts/r17/t1_sitl.sh
+++ scripts/r17/t1_sitl.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd ~/ardupilot/ArduCopter
-sim_vehicle.py -L Ballarat --mavproxy-args "--out=udpin:0.0.0.0:14552 --state-basedir=/usr/local/home/bsr8w/flightlog --aircraft=copter" --console --map
+pushd ~/ardupilot/ArduCopter
+sim_vehicle.py -v ArduCopter -L Ballarat --mavproxy-args "--out=udpin:0.0.0.0:14552 --state-basedir=/usr/local/home/bsr8w/flightlog --aircraft=copter" --console --map -w
+popd
