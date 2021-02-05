294fe64f7e4b935ee25503041704727ec0b04938
Bo Shang <cnpcshangbo@gmail.com>
2021-02-05 02:41:57 -0600

    increased version number, used python3 to compile pyrealsense, updated pythonpath

--- scripts/launchT265.bash
+++ scripts/launchT265.bash
@@ -1,4 +1,4 @@
 #/bin/bash
 echo "Run t265 scripts."
 # docker exec -it rs-zed-container bash -c "source devel/setup.bash && cd /root/catkin_ws && source devel/setup.bash && rosrun beginner_tutorials velocity_based_movement.py"
-docker exec -it rs-zed-container bash /root/code/vision_to_mavros/scripts/t265.sh
+docker exec -it rs-zed-container bash -c "export PYTHONPATH=$PYTHONPATH:/usr/local/lib:/usr/local/lib/python3.6/pyrealsense2 && source /root/code/vision_to_mavros/scripts/t265.sh"
